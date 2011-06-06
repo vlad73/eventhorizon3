b93fb6e3fca3c44f86dd5677304205c4fca50571
vlad73 <voskoboynikvlad@gmail.com>
2011-06-07 00:34:21 +0300

    fix csrf_exempt

--- gateway/services.py
+++ gateway/services.py
@@ -1,24 +1,50 @@
-# -*- coding: UTF-8 -*-
-
-from google.appengine.api import xmpp
-from gateway.server_protocol import extract_patterns
-from django.views.decorators.csrf import csrf_exempt
 import logging
-from django.utils import simplejson
-
-#noinspection PyUnresolvedReferences
-def handle_message(json):
-    logging.info("Received message: %s" % json)
-    data = simplejson.loads(json)
-    action = data['action']
-    if action == 'extract_patterns':
-        result = extract_patterns(data)
-        result.append({'discussion_id': data['discussion_id']})
-    else:
-        result = "could not understand your request"
-    return simplejson.dumps(result)
+from google.appengine.api import xmpp
+from google.appengine.api.mail_errors import InvalidEmailError
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+from google.appengine.api.mail import send_mail
 
 @csrf_exempt
-def on_message(request):
-    message = xmpp.Message(request.POST)
-    message.reply(handle_message(message.body))
+def send_notification_message(request):
+    if request.POST:
+        # perform GET
+        protocol = request.GET.get("protocol","email")
+        if protocol == "email":
+            try:
+                send_email_message(request)
+                return HttpResponse("message was sent successfully!")
+            except InvalidEmailError:
+                return HttpResponse("wrong email address")
+
+
+            #send_xmpp_message(request)
+
+def send_email_message(request):
+     # normalize the given amount
+    sender = request.POST.get("sender")
+    to = request.POST.get("to")
+    subject = request.POST.get("subject")
+    body  = request.POST.get("body")
+    logging.info('mail from %s to %s body %s' % (sender,to, body))
+    send_mail(sender, to, subject, body)
+
+def send_xmpp_message(request):
+    to = request.POST.get("to")
+    sender = request.POST.get("sender")
+    msg  = request.Post.get("body")
+    chat_message_sent = False
+
+    if xmpp.get_presence(to):
+        status_code = xmpp.send_message(to, msg, sender)
+        chat_message_sent = (status_code != xmpp.NO_ERROR)
+
+    if not chat_message_sent:
+        # Send an email message instead...
+        return HttpResponse("message error!")
+    else:
+        return HttpResponse("message was sent successfully!")
+
+
+
+
